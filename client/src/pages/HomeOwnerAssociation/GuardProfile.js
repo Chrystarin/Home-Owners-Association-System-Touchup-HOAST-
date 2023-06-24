8aecb1f84c8bf861fd6910d93963da3bcd18e903
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-25 05:05:32 +0800

    Update GuardProfile.js

--- client/src/pages/HomeOwnerAssociation/GuardProfile.js
+++ client/src/pages/HomeOwnerAssociation/GuardProfile.js
@@ -5,34 +5,36 @@ import Avatar from '@mui/material/Avatar';
 import axios from '../../utils/axios';
 import Button from '@mui/material/Button';
 import { useParams } from 'react-router-dom';
-function Profile() {
-    const {id} = useParams();
-    const [user, setUser] = useState();
+import {useAuth} from './../../utils/AuthContext.js';
+function GuardProfile() {
+    const [guards, setGuards] = useState();
+    const {isRole} = useAuth();
 
     // Runs onLoad
 	useEffect(() => {
-		const fetchUser = async () => {
+		const fetchGuards = async () => {
 			await axios
-				.get(`users`, {
-                    params:{
-                        id: id,
-                    }
-                })
+				.get(`hoas/guards`, {
+					params: {
+						hoaId: localStorage.getItem('hoaId'),
+                        hoaId: (isRole('admin') || isRole('guard')) ? localStorage.getItem('hoaId') : null
+					}
+				})
 				.then((response) => {
-                    setUser(response.data)
-					console.log(response.data);
+                    console.log(response.data)
+					setGuards(response.data)
 				});
 		};
-		fetchUser();
+        fetchGuards();
 	}, []);
 
-    if (!user) return <div>Loading...</div>
+    if (!guards) return <div>Loading...</div>
     
     return <>
         <Navbar/>
         <div className='SectionHolder'>
             <section className='Section'>
-            <h3 className='SectionTitleDashboard'><span><a href="/homes">My Profile</a></span>  > <span>{user.name.firstName + " " + user.name.lastName}</span></h3>
+            {/* <h3 className='SectionTitleDashboard'><span><a href="/homes">My Profile</a></span>  > <span>{user.name.firstName + " " + user.name.lastName}</span></h3> */}
                 <div className='SectionContent SectionView' id='ViewResident'>
                     <div className='SectionView__Content' id="ViewResident__Content__Container" >
                         <div className="SectionView__Sections">
@@ -43,15 +45,15 @@ function Profile() {
                                 <Avatar sx={{ width: 76, height: 76 }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                                 <div className='GeneralInformation__InfoContainer'>
                                     <h6>Name:</h6>
-                                    <h5>{user.name.firstName + " " + user.name.lastName}</h5>
+                                    {/* <h5>{user.name.firstName + " " + user.name.lastName}</h5> */}
                                     <h6>User ID:</h6>
-                                    <h5>{user.userId}</h5>
+                                    {/* <h5>{user.userId}</h5> */}
                                 </div>
                                 <div className='GeneralInformation__InfoContainer'>
                                     <h6>Email: </h6>
-                                    <h5>{user.email}</h5>
+                                    {/* <h5>{user.email}</h5> */}
                                     <h6>Registered Since: </h6>
-                                    <h5>{new Date(user.createdAt).toLocaleString('default', { month: 'long' }) + " " + new Date(user.createdAt).getDate() + ", " + new Date(user.createdAt).getFullYear() }</h5>
+                                    {/* <h5>{new Date(user.createdAt).toLocaleString('default', { month: 'long' }) + " " + new Date(user.createdAt).getDate() + ", " + new Date(user.createdAt).getFullYear() }</h5> */}
                                 </div>
                                 <Button variant="contained" href='/profile/edit'>Edit Profile</Button>
 
@@ -64,4 +66,4 @@ function Profile() {
     </>
 }
 
-export default Profile
+export default GuardProfile
