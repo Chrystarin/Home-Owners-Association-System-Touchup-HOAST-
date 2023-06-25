6d4bcae2a65d0ef09119961dbbaec6655b29ddf5
haroldcastillo <44398016+haroldcastillo@users.noreply.github.com>
2023-06-25 16:40:05 +0800

    guard profile

--- client/src/pages/HomeOwnerAssociation/GuardProfile.js
+++ client/src/pages/HomeOwnerAssociation/GuardProfile.js
@@ -7,28 +7,34 @@ import Button from '@mui/material/Button';
 import { useParams } from 'react-router-dom';
 import {useAuth} from './../../utils/AuthContext.js';
 function GuardProfile() {
-    const [guards, setGuards] = useState();
+    const {id} = useParams();
+    const [guard, setGuard] = useState();
     const {isRole} = useAuth();
 
     // Runs onLoad
 	useEffect(() => {
-		const fetchGuards = async () => {
-			await axios
-				.get(`hoas/guards`, {
-					params: {
-						hoaId: localStorage.getItem('hoaId'),
-                        hoaId: (isRole('admin') || isRole('guard')) ? localStorage.getItem('hoaId') : null
-					}
-				})
-				.then((response) => {
-                    console.log(response.data)
-					setGuards(response.data)
-				});
-		};
         fetchGuards();
 	}, []);
 
-    if (!guards) return <div>Loading...</div>
+    const fetchGuards = async () => {
+        await axios
+            .get(`hoas/guards`, {
+                params: {
+                    hoaId: localStorage.getItem('hoaId'),
+                    hoaId: (isRole('admin') || isRole('guard')) ? localStorage.getItem('hoaId') : null
+                }
+            })
+            .then((response) => {
+                console.log(JSON.stringify(response.data))
+                console.log(response.data)
+                const getUserById = (userId) => {
+                    return (response.data).find(item => item.user.userId === userId);
+                };
+                setGuard(getUserById(id));
+            });
+    };
+
+    if (!guard) return <div>Loading...</div>
     
     return <>
         <Navbar/>
@@ -45,17 +51,16 @@ function GuardProfile() {
                                 <Avatar sx={{ width: 76, height: 76 }} alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
                                 <div className='GeneralInformation__InfoContainer'>
                                     <h6>Name:</h6>
-                                    {/* <h5>{user.name.firstName + " " + user.name.lastName}</h5> */}
+                                    <h5>{guard.user.name.firstName + " " + guard.user.name.lastName}</h5>
                                     <h6>User ID:</h6>
-                                    {/* <h5>{user.userId}</h5> */}
+                                    <h5>{guard.user.userId}</h5>
                                 </div>
                                 <div className='GeneralInformation__InfoContainer'>
-                                    <h6>Email: </h6>
-                                    {/* <h5>{user.email}</h5> */}
-                                    <h6>Registered Since: </h6>
-                                    {/* <h5>{new Date(user.createdAt).toLocaleString('default', { month: 'long' }) + " " + new Date(user.createdAt).getDate() + ", " + new Date(user.createdAt).getFullYear() }</h5> */}
+                                    <h6>Status: </h6>
+                                    <h5>{guard.status}</h5>
+                                    <h6>Hired Since: </h6>
+                                    <h5>{new Date(guard.hiredAt).toLocaleString('default', { month: 'long' }) + " " + new Date(guard.hiredAt).getDate() + ", " + new Date(guard.hiredAt).getFullYear() }</h5>
                                 </div>
-                                <Button variant="contained" href='/profile/edit'>Edit Profile</Button>
 
                             </div>
                         </div>
