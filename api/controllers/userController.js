7055bf0b194e1b94e79a4756bf0f172747b902e4
Gian Carlo Dela Cruz <59326022+Deuz1007@users.noreply.github.com>
2023-06-26 06:37:03 +0800

    Update userController.js

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -11,143 +11,130 @@ const Home = require('../models/Home');
 const sendEmail = require('../helpers/sendEmail');
 
 const createToken = (userId) =>
-	jwt.sign({ userId, createdAt: new Date() }, JWT_SECRET, {
-		expiresIn: '7d'
-	});
+    jwt.sign({ userId, createdAt: new Date() }, JWT_SECRET, {
+        expiresIn: '7d'
+    });
 const cookieOptions = { httpOnly: true, sameSite: 'none', secure: true };
 
 const signup = async (req, res, next) => {
-	const { firstName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
+    
+    checkString(firstName, 'First Name');
+    checkString(lastName, 'Last Name');
+    checkString(password, 'Password');
+    checkEmail(email);
 
-	checkString(firstName, 'First Name');
-	checkString(lastName, 'Last Name');
-	checkString(password, 'Password');
-	checkEmail(email);
+    // Create user
+    const user = await User.create({
+        userId: genUserId(),
+        name: { firstName, lastName },
+        credentials: { email, password: await bcrypt.hash(password, 10) }
+    });
 
-	// Create user
-	const user = await User.create({
-		userId: genUserId(),
-		name: { firstName, lastName },
-		credentials: { email, password: await bcrypt.hash(password, 10) }
-	});
-
-	res.status(201).json({ message: 'Account created', userId: user.userId });
+    res.status(201).json({ message: 'Account created', userId: user.userId });
 };
 
 const login = async (req, res, next) => {
-	const { email, password } = req.body;
+    const { email, password } = req.body;
 
-	checkEmail(email);
-	checkString(password, 'Password');
+    checkEmail(email);
+    checkString(password, 'Password');
 
-	// Find email
-	const user = await User.findOne({ 'credentials.email': email }).exec();
-	if (!user) throw new UnauthorizedError('Incorrect email');
+    // Find email
+    const user = await User.findOne({ 'credentials.email': email }).exec();
+    if (!user) throw new UnauthorizedError('Incorrect email');
 
-	// Check password
-	const samePassword = await bcrypt.compare(
-		password,
-		user.credentials.password
-	);
-	if (!samePassword) throw new UnauthorizedError('Incorrect password');
+    // Check password
+    const samePassword = await bcrypt.compare(password, user.credentials.password);
+    if (!samePassword) throw new UnauthorizedError('Incorrect password');
 
-	res.status(201)
-		.cookie('access-token', createToken(user.userId), cookieOptions)
-		.json({ user: user });
+    res.status(201).cookie('access-token', createToken(user.userId), cookieOptions).json({ user: user });
 };
 
 const getUser = async (req, res, next) => {
-	const { user } = req.user;
-	const {
-		credentials: { email }
-	} = user;
+    const { user } = req.user;
+    const {
+        credentials: { email }
+    } = user;
 
-	res.json({ ...user.toJSON(), email });
+    res.json({ ...user.toJSON(), email });
 };
 
 const updateUser = async (req, res, next) => {
-	const { firstName, lastName, email, password } = req.body;
+    const { firstName, lastName, email, password } = req.body;
 
-	// Update user
-	user = {
-		...user,
-		name: { firstName, lastName },
-		credentials: { email, password }
-	};
-	await user.save();
+    // Update user
+    user = {
+        ...user,
+        name: { firstName, lastName },
+        credentials: { email, password }
+    };
+    await user.save();
 
-	res.json({ message: 'User updated' });
+    res.json({ message: 'User updated' });
 };
 
 const addHomeowner = async (req, res, next) => {
-	const {
-		resident: { firstName, lastName, email, contactNo },
-		home: { homeNo, street, phase }
-	} = req.body;
-	const { hoa } = req.user;
+    const {
+        resident: { firstName, lastName, email, contactNo },
+        home: { homeNo, street, phase }
+    } = req.body;
+    const { hoa } = req.user;
 
-	const genPass = genPassword();
 
-	// Create user
-	const homeowner = await User.create({
-		userId: genUserId(),
-		name: { firstName, lastName },
-		credentials: { email, password: await bcrypt.hash(genPass, 10) }
-	});
+    const genPass = genPassword();
 
-	// Create home
-	const home = await Home.create({
-		homeId: genHomeId(),
-		name: lastName + "'s Residence",
-		owner: homeowner._id,
-		hoa: hoa._id,
-		address: { number: homeNo, street, phase },
-		contactNo,
-		residents: [{ user: homeowner._id }]
-	});
+    // Create user
+    const homeowner = await User.create({
+        userId: genUserId(),
+        name: { firstName, lastName },
+        credentials: { email, password: await bcrypt.hash(genPass, 10) }
+    });
 
-	res.status(201).json({
-		message: 'Homeowner and Home added',
-		homeowner,
-		home,
-		credentials: {
-			email,
-			password: genPass
-		}
-	});
+    // Create home
+    const home = await Home.create({
+        homeId: genHomeId(),
+        name: lastName + "'s Residence",
+        owner: homeowner._id,
+        hoa: hoa._id,
+        address: { number: homeNo, street, phase },
+        contactNo,
+        residents: [{ user: homeowner._id }]
+    });
+
+    res.status(201).json({
+        message: 'Homeowner and Home added',
+        homeowner,
+        home,
+        credentials: {
+            email,
+            password: genPass
+        }
+    });
 };
 
 const forgetPassword = async (req, res, next) => {
-	const { email, password } = req.body;
-	const user = await User.findOne({ email }).exec();
-	if (!user) new UnauthorizedError('User not found');
+    const { email, password } = req.body;
+    const user = await User.findOne({ 'credentials.email': email }).exec();
 
-	await User.findOneAndUpdate(
-		{ email },
-		{ $set: { 'credentials.password': bcrypt.hashSync(password, 10) } }
-	);
+    if(!user) new UnauthorizedError('User not found');
 
-	res.json({ message: 'Password Reset Successfully' });
+    user.credentials.password = bcrypt.hashSync(password, 10);
+    await user.save();
+
+    res.json({ message: 'Password Reset Successfully' });
 };
 
 const sendMail = async (req, res, next) => {
-	const { email, message } = req.body;
+    const { email, message } = req.body;
 
-	console.log(email, message);
+    console.log(email, message);
 
-	checkEmail(email);
+    checkEmail(email);
 
-	await sendEmail(email, message);
+    await sendEmail(email, message);
+    
+    res.json({ message: 'Email sent' });
+}
 
-	res.json({ message: 'Email sent' });
-};
-
-module.exports = {
-	signup,
-	login,
-	getUser,
-	updateUser,
-	forgetPassword,
-	sendMail,
-	addHomeowner
-};
+module.exports = { signup, login, getUser, updateUser, forgetPassword, sendMail, addHomeowner };
